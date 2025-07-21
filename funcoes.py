6ee242ba9fa26bf7f0d14cfd74e0c9f9eb6ae28d
heitorleme <78544347+heitorleme@users.noreply.github.com>
2025-07-21 18:03:01 +0100

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def extrair_elementos(nome_ficheiro):
     if not isinstance(nome_ficheiro, str) or nome_ficheiro.strip() == "":
         return "Desconhecido", "Desconhecido", "Desconhecido"
@@ -11,3 +13,9 @@ def extrair_elementos(nome_ficheiro):
 
     marca, intervalo, tipo = partes
     return marca, intervalo, tipo
+
+# Criar coluna para categoria do influencer
+def atribuir_influ_category(df):
+    df["influ_category"] = pd.cut(df["Total Connections"],
+                                bins = [0, 10000, 200000, 800000, 2900000, float("Inf")],
+                                labels = ["Nano", "Micro", "Mid", "Macro", "Hero"])
